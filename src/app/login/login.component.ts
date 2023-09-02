6cdb94845bca53ac210d83b23633e2c04260f63c
Mohd Amaan <mohd.amaan45@gmail.com>
2023-09-02 20:19:28 +0530

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,43 +14,34 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   user: Signup = new Signup();
   msg: string='';
   constructor(private route : Router, private common: ServiceService,private toastr : ToastrService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit1(){
-    this.toastr.success("Hello Wordl ","for fun")
+  onSubmit1() {
+    this.toastr.success('Hello Wordl ', 'for fun');
   }
   onSubmit() {
-    this.common.httpPost(LOGIN, this.user).subscribe( {
-      next: data => {
-        
-        // if(data.response.role[0].role=="admin"){
-        //   this.common.showMessage("Admin Login Successfully",true);
-        //   this.route.navigate(['home']);
-        // }
-        // if(data.response.role[0].role=="user"){
-        //   this.common.showMessage("User Login Successfully",true);
-        //   this.route.navigate(['home']);
-        // }
-        this.toastr.success(data.response.role[0].roleDes,"Login Sucessfully");
-        this.common.setLoggedIn();
+    this.common.httpPost(LOGIN, this.user).subscribe({
+      next: (data) => {
+        this.toastr.success(data.response.role[0].roleDes, 'Login Sucessfully');
+        this.common.setLoggedIn(); //For login
+
+        const role = data.response.role[0].role;
+        this.common.setRoles(role); //  For getting roles
         this.route.navigate(['main']);
-         },
-      error: error => {
-       this.toastr.error(error.error.message,"Login Failed",);
+      },
+      error: (error) => {
+        this.toastr.error(error.error.message, 'Login Failed');
         // if (error instanceof HttpErrorResponse) {
-        
+
         // }
-      }
+      },
     } as Observer<ResponseDto>);
   }
-  
-  
 }
