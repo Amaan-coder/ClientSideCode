0260e058fb43d24ff93063f1ff3ea162b8c9e5f7
Mohd Amaan <mohd.amaan45@gmail.com>
2023-09-04 23:48:00 +0530

    all validation done

--- src/app/employee/addemployee/addemployee.component.ts
+++ src/app/employee/addemployee/addemployee.component.ts
@@ -5,7 +5,7 @@ import { ServiceService } from 'src/common/service.service';
 import { EmployeeComponent } from '../employee.component';
 import { ADD_EMPLOYEE } from 'src/auth';
 import { ToastrService } from 'ngx-toastr';
-import { AbstractControl, FormControl, Validators } from '@angular/forms';
+import { AbstractControl, Form, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addemployee',
@@ -19,18 +19,21 @@ export class AddemployeeComponent implements OnInit {
   id: any;
   employee: Employee = new Employee();
 
-  firstName = new FormControl('', Validators.required);
-  lastName = new FormControl('', Validators.required);
+  firstName = new FormControl('', [Validators.required, this.alphabetValidator]);
+  lastName = new FormControl('', [Validators.required, this.alphabetValidator]);
+
   add1 = new FormControl("", Validators.required);
   add2 = new FormControl("", Validators.required);
-  city = new FormControl("", Validators.required);
-  state = new FormControl("", Validators.required);
-  phone = new FormControl("", [Validators.required, this.numericValidator]);
+  city = new FormControl('', [Validators.required, this.alphabetValidator]);
+  state = new FormControl('', [Validators.required, this.alphabetValidator]);
+
+  phone = new FormControl(null, [Validators.required, this.numericValidator]);
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
   pin = new FormControl("", [Validators.required, this.numericValidator]);
-  company = new FormControl("", Validators.required);
+  company = new FormControl('', [Validators.required, this.alphabetValidator]);
+
 
 
   ngOnInit(): void {
@@ -49,7 +52,8 @@ export class AddemployeeComponent implements OnInit {
         this.router.navigate(['/main/employeeList']);
       })
     }
-    else{
+    else {
+      this.toastr.warning("Please fill the mandatory fields or Invalid Fields");
       this.firstName.markAsTouched();
       this.firstName.markAsDirty();
       this.lastName.markAsDirty();
@@ -70,23 +74,35 @@ export class AddemployeeComponent implements OnInit {
       this.company.markAsTouched();
       this.email.markAsDirty();
       this.email.markAsTouched();
-      
-      this.toastr.warning("Please Fill the mandatory Fields");
-    }
+}
+}
 
-
-
-  }
-
-     numericValidator(control: AbstractControl): { [key: string]: any } | null {
+  numericValidator(control: AbstractControl): { [key: string]: any } | null {
     const valid = /^\d+$/.test(control.value);
     return valid ? null : { nonNumeric: true };
   }
-  numberChange(event:Event){
+  numberChange(event: Event) {
     const i = event.target as HTMLInputElement;
-    let num= i.value;
-    num = num.replace(/\D/g,'');
-    num = num.slice(0 ,10);
+    let num = i.value;
+    num = num.replace(/\D/g, '');
+    num = num.slice(0, 10);
     i.value = num;
   }
+
+  forAlphabetOnly(name: string): boolean {
+    const pattern = /^[a-zA-Z\s]*$/;
+    return pattern.test(name);
+  }
+
+  alphabetValidator(control: FormControl): { [key: string]: any } | null {
+    const alphabeticCharacters = /^[a-zA-Z\s]*$/; // Allow alphabetic characters and spaces
+    if (control.value && !alphabeticCharacters.test(control.value)) {
+      return { invalidAlphabet: true };
+    }
+    return null;
+  }
+
+
+
+
 }
--- src/app/employee/editemployee/editemployee.component.ts
+++ src/app/employee/editemployee/editemployee.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ServiceService } from 'src/common/service.service';
 import { ADD_EMPLOYEE, EMPLOYEE_DETAILS } from 'src/auth';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editemployee',
@@ -15,6 +16,21 @@ export class EditemployeeComponent implements OnInit {
   id:any;
   employee:Employee=new Employee();
   employeeList: any=[];
+  firstName = new FormControl('', [Validators.required, this.alphabetValidator]);
+  lastName = new FormControl('', [Validators.required, this.alphabetValidator]);
+
+  add1 = new FormControl("", Validators.required);
+  add2 = new FormControl("", Validators.required);
+  city = new FormControl('', [Validators.required, this.alphabetValidator]);
+  state = new FormControl('', [Validators.required, this.alphabetValidator]);
+
+  phone = new FormControl(null, [Validators.required, this.numericValidator]);
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  pin = new FormControl("", [Validators.required, this.numericValidator]);
+  company = new FormControl('', [Validators.required, this.alphabetValidator]);
+
   constructor(private router:Router, private common:ServiceService, private route:ActivatedRoute,private toastr:ToastrService) { }
 
   ngOnInit(): void {
@@ -34,11 +50,61 @@ export class EditemployeeComponent implements OnInit {
     }
   })
  }
- onSubmit(){
-  this.common.httpPost(ADD_EMPLOYEE,this.employee).subscribe(data=>{
-    this.toastr.success("Employee Edited Successfully");
-    this.router.navigate(['/main/employeeList']);
-  })
+  onSubmit() {
+    if (this.firstName.valid && this.lastName.valid && this.add1.valid && this.add2.valid && this.city.valid && this.state.valid && this.phone.valid && this.email.valid && this.pin.valid && this.company.valid) {
+      this.common.httpPost(ADD_EMPLOYEE, this.employee).subscribe(data => {
+        this.toastr.success("Employee Edited Successfully");
+        this.router.navigate(['/main/employeeList']);
+      })
+    }
+    else {
+      this.toastr.warning("Please fill the mandatory fields or Invalid Fields");
+      this.firstName.markAsTouched();
+      this.firstName.markAsDirty();
+      this.lastName.markAsDirty();
+      this.lastName.markAsTouched();
+      this.add1.markAsDirty();
+      this.add1.markAsTouched();
+      this.add2.markAsDirty();
+      this.add2.markAsTouched();
+      this.city.markAsDirty();
+      this.city.markAsTouched();
+      this.state.markAsDirty();
+      this.state.markAsTouched();
+      this.pin.markAsDirty();
+      this.pin.markAsTouched();
+      this.phone.markAsDirty();
+      this.phone.markAsTouched();
+      this.company.markAsDirty();
+      this.company.markAsTouched();
+      this.email.markAsDirty();
+      this.email.markAsTouched();
+    }
+  }
 
+numericValidator(control: AbstractControl): { [key: string]: any } | null {
+  const valid = /^\d+$/.test(control.value);
+  return valid ? null : { nonNumeric: true };
 }
+numberChange(event: Event) {
+  const i = event.target as HTMLInputElement;
+  let num = i.value;
+  num = num.replace(/\D/g, '');
+  num = num.slice(0, 10);
+  i.value = num;
+}
+
+forAlphabetOnly(name: string): boolean {
+  const pattern = /^[a-zA-Z\s]*$/;
+  return pattern.test(name);
+}
+
+alphabetValidator(control: FormControl): { [key: string]: any } | null {
+  const alphabeticCharacters = /^[a-zA-Z\s]*$/; // Allow alphabetic characters and spaces
+  if (control.value && !alphabeticCharacters.test(control.value)) {
+    return { invalidAlphabet: true };
+  }
+  return null;
+}
+
 }
